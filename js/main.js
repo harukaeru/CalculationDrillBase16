77f3276b6f543286d95237039d7a987a911c4fd9
NakadaTakumi <usrneko@harukaeru.local>
2018-04-01 21:30:10 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,37 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Generator from './Generator'
-import { plusConfig } from './configs'
 import Config from './components/Config'
-import Table from './components/Table'
-
-
-// View
-class Solver {
-  constructor(problemScheme) {
-    this.problemScheme = problemScheme
-  }
-
-  ask() {
-    console.log(this.problemScheme.getDisplay())
-    const userAnswer = '23'
-    const formedUserAnswer = userAnswer.replace('\n', '')
-
-    if (formedUserAnswer == this.problemScheme.getAnswer()) {
-      console.log("CORRECT!!")
-    } else {
-      console.log(`boo. Answer is ${this.problemScheme.getAnswer()}`)
-    }
-  }
-}
-
-// Use
 
 
 ReactDOM.render(
-  <div>
-    <Config />
-  </div>,
+  <Config />,
   document.getElementById('root')
 )
 
